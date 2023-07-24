4d8858f94376376ca4425e92bc9204f943090b13
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-24 18:49:04 -0300

    create CabinTable component using useQuery hook

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -23,3 +23,35 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
+
+const CabinTable = () => {
+  const { isLoading, data: cabins, error } = useQuery({
+    queryKey: ['cabin'],
+    queryFn: getCabins
+  })
+
+  if (isLoading) return <Spinner />
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map(cabin => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  )
+}
+
+export default CabinTable
