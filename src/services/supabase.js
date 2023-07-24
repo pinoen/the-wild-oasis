2a4e1c2ba115aa0bd0914fd9071925bb611c6600
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-24 16:55:05 -0300

    Add supabase file so to config api and connect it with frontend

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://xvcjrmdztxtrrudzavok.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2Y2pybWR6dHh0cnJ1ZHphdm9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAxMjU1NDYsImV4cCI6MjAwNTcwMTU0Nn0.5qWseqnU9VEuGYJ70JO8fCQhpLg96fsa3Cypivpjp80'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
