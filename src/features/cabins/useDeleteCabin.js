2bbe5ee378a71981a3b44d32826238dad2282620
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-25 22:30:51 -0300

    fix bug deleteCabin as deleteCabinApi

--- src/features/cabins/useDeleteCabin.js
+++ src/features/cabins/useDeleteCabin.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { deleteCabinApi } from "../../services/apiCabins"
+import { deleteCabin as deleteCabinApi } from "../../services/apiCabins"
 import { toast } from "react-hot-toast"
 
 
