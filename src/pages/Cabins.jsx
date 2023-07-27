2b4bc1f9e6f68103e488d3969651b8741584a37f
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-27 15:50:02 -0300

    refactor so to use addcabin component

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,13 +2,10 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from '../ui/Button'
-import { useState } from "react";
-import CreateCabinForm from '../features/cabins/CreateCabinForm'
+import AddCabin from "../features/cabins/AddCabin";
 
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false)
 
   return (
     <>
@@ -21,8 +18,7 @@ function Cabins() {
       <Row>
         <CabinTable />
 
-        <Button onClick={() => setShowForm(curr => !curr)}>Add new cabin</Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
