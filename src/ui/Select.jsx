8b99217111e95ad836f6ba4217e877a3af23baf0
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-02 17:36:32 -0300

    create select component so to return the available options for sorting cabins

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import styled from "styled-components";
 
 const StyledSelect = styled.select`
@@ -5,11 +6,24 @@ const StyledSelect = styled.select`
   padding: 0.8rem 1.2rem;
   border: 1px solid
     ${(props) =>
-      props.type === "white"
-        ? "var(--color-grey-100)"
-        : "var(--color-grey-300)"};
+    props.type === "white"
+      ? "var(--color-grey-100)"
+      : "var(--color-grey-300)"};
   border-radius: var(--border-radius-sm);
   background-color: var(--color-grey-0);
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `;
+
+const Select = ({ options, value, onChange }) => {
+  return (
+    <StyledSelect value={value} onChange={onChange}>
+      {options.map(option => (
+        <option key={option.value} value={option.value}>{option.label}</option>
+      ))}
+
+    </StyledSelect>
+  )
+}
+
+export default Select
