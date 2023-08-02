dc21058559f8013ad51acf0e62a7b4071f80e63c
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-02 14:06:10 -0300

    add filtering conditions

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
+import { useSearchParams } from "react-router-dom";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -30,8 +31,17 @@ const TableHeader = styled.header`
 const CabinTable = () => {
   // eslint-disable-next-line no-unused-vars
   const { isLoading, cabins, error } = useCabins()
+  const [searchParams] = useSearchParams()
 
   if (isLoading) return <Spinner />
+
+  const filterValue = searchParams.get('discount') || 'all'
+
+  let filteredCabins;
+
+  if (filterValue === 'all') filteredCabins = cabins;
+  if (filterValue === 'no-discount') filteredCabins = cabins.filter(cabin => cabin.discount === 0)
+  if (filterValue === 'with-discount') filteredCabins = cabins.filter(cabin => cabin.discount > 0)
 
   return (
     <Table role="table">
@@ -43,7 +53,7 @@ const CabinTable = () => {
         <div>Discount</div>
         <div></div>
       </TableHeader>
-      {cabins.map(cabin => (
+      {filteredCabins.map(cabin => (
         <CabinRow cabin={cabin} key={cabin.id} />
       ))}
     </Table>
