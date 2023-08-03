bf4306312d0bf7c14dad5e7cb2e58a35fbb860e5
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-03 12:21:31 -0300

    getbookings function receives sortby object to perform the sorting from the api

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -2,12 +2,16 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 
-export async function getBookings({ filter }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from('bookings').select("*, cabins(name), guests(fullName, email)")
 
-  if (filter !== null) {
+  if (filter) {
     query.eq(filter.field, filter.value)
+  }
+
+  if (sortBy) {
+    query.order(sortBy.field, { ascending: sortBy.direction === 'asc' })
   }
 
   const { data, error } = await query
