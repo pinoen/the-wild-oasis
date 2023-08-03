807d6bb25c5118a32b6b8f9231e7a8a270c08707
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-03 11:59:12 -0300

    function getbookings receives filter object

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -2,8 +2,15 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 
-export async function getBookings() {
-  const { data, error } = await supabase.from('bookings').select("*, cabins(name), guests(fullName, email)")
+export async function getBookings({ filter }) {
+  let query = supabase
+    .from('bookings').select("*, cabins(name), guests(fullName, email)")
+
+  if (filter !== null) {
+    query.eq(filter.field, filter.value)
+  }
+
+  const { data, error } = await query
 
   if (error) {
     console.log(error)
