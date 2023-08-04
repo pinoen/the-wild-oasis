8730218069768901e8ba7157a77b886dbd8f304e
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-04 11:15:30 -0300

    takes count from useBookings so to use it in pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -7,10 +7,9 @@ import Spinner from "../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings()
+  const { bookings, isLoading, count } = useBookings()
 
   if (isLoading) return <Spinner />
-
   if (!bookings.length) return <Empty resource='bookings' />
 
   return (
@@ -32,7 +31,7 @@ function BookingTable() {
           )}
         />
         <Table.Footer>
-          <Pagination count={45} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
