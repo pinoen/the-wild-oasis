5d7f3a8b9efb332f14c83a4638737d99db48082d
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-08 11:32:17 -0300

    add logic to signup new users

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,19 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm()
+  const { register, formState, getValues, handleSubmit, reset } = useForm()
   const { errors } = formState
+  const { signup, isLoading } = useSignup()
 
-  function onSubmit(data) {
-    console.log(data)
+  function onSubmit({ fullName, email, password }) {
+    signup(fullName, email, password, {
+      onSettled: reset
+    })
   }
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
